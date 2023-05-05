4a5d8fba2c1197873cf1adb3cf563e953ea4fbe6
lucas-ribard <lucas.ribard@laplateforme.io>
2023-05-05 15:52:53 +0200

    ADDED : buttons for books

--- src/View/index.php
+++ src/View/index.php
@@ -1,5 +1,7 @@
 <script defer src=index.js></script>
 <br>
 <button id="BTusers" onclick="FetchContent('users')">tout les users</button>
+<button id="BTusers" onclick="FetchContent('books')">tout les livres</button>
+
 <main id='Main'>
 </main>
